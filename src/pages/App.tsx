d860757bc9b7df4778ea0067f158d4997a1579f4
Marien van Overbeek <git@marienvanoverbeek.nl>
2023-10-07 11:38:43 +0200

    Add todos

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default () => {
     return (
         <div>
-            <h1>Test ok sdf</h1>
+            <h1>Todo</h1>
+            <ul>
+                <li>Replace favicon</li>
+                <li>Add components from current CV site</li>
+                <li>Add downloadable PDF for recruiters to site  <ul>
+                    <li>Can this PDF be automatically generated from same content as used on site?</li>
+                </ul></li>
+            </ul>
         </div>
     )
 }
