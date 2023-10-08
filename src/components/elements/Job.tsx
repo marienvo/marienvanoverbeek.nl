cf73a942feb380ebb26092d4483b0a0e3e70802a
Marien van Overbeek <git@marienvanoverbeek.nl>
2023-10-08 13:25:18 +0200

    Fixup CSS experience

--- src/components/elements/Job.tsx
+++ src/components/elements/Job.tsx
@@ -3,24 +3,29 @@ import cv from "../../../content/cv.json";
 import { getTime } from "./../helpers/getTime.ts";
 import { Heading } from "./Heading.tsx";
 
-export const Job: FC<JobProperties> = ({ job }) => {
+export const Job: FC<JobProperties> = ({ job, isLast }) => {
   const { startDate, endDate, duration } = getTime(job);
   return (
-    <li className="mb-10 ml-4">
-      <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-2 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-      <Heading level="h3">
-        {job.company} - {job.position}
-      </Heading>
-      <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-        {startDate} – {endDate}
-        {duration}, {job.location}
-      </time>
-      <p className="mb-2 text-base font-normal text-gray-500 dark:text-gray-400">
-        {job.description}
-      </p>
-      <p className="mb-4 text-sm font-normal text-gray-500 dark:text-gray-400">
-        {job.skills.join(" · ")}
-      </p>
+    <li className="mb-10 ml-4 relative">
+      <div className="absolute w-[11px] h-[11px] bg-gray-200 rounded-full -left-[22px] border-2 border-gray-50 dark:border-gray-900 dark:bg-gray-700"></div>
+      <div className="relative -top-2">
+        <Heading level="h3">
+          {job.company} - {job.position}
+        </Heading>
+        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+          {startDate} – {endDate}
+          {duration}, {job.location}
+        </time>
+        <p className="mb-2 text-base font-normal text-gray-500 dark:text-gray-400">
+          {job.description}
+        </p>
+        <p className="mb-4 text-sm font-normal text-gray-500 dark:text-gray-400">
+          {job.skills.join(" · ")}
+        </p>
+      </div>
+      {isLast && (
+        <div className="absolute top-3 h-full bg-gray-50 w-3 -left-[22px]"></div>
+      )}
     </li>
   );
 };
@@ -31,4 +36,5 @@ export type Job = CV["jobs"][number];
 
 type JobProperties = {
   job: Job;
+  isLast: boolean;
 };
--- src/components/elements/Timeline.tsx
+++ src/components/elements/Timeline.tsx
@@ -5,8 +5,8 @@ import { Job } from "./Job.tsx";
 export const Timeline = () => {
   return (
     <ol className="relative border-l border-gray-200 dark:border-gray-700 mt-8">
-      {cv.jobs.map((job) => (
-        <Job job={job} />
+      {cv.jobs.map((job, index) => (
+        <Job job={job} isLast={index === cv.jobs.length - 1} />
       ))}
     </ol>
   );
