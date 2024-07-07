680b99ea4e7f5d10ad4ba48af9600aaefa7df41e
Marien van Overbeek <git@marienvanoverbeek.nl>
2024-07-07 20:56:18 +0200

    Update some content

--- src/components/RecipeDetail.tsx
+++ src/components/RecipeDetail.tsx
@@ -15,11 +15,14 @@ export default ({ recipe }: { recipe: Recipe }) => {
           <br />
           No Spot
         </Heading>
-        <a href={`/${settings.recipeSlug}`}>Back to all recipes</a>
         <Heading level="h2">{recipe.title}</Heading>
         <div className="max-w-[400px]">
           <ReactMarkdown>{recipe.intro}</ReactMarkdown>
         </div>
+        <br />
+        <a href={`/${settings.recipeSlug}`}>
+          <u>« Back to all recipes</u>
+        </a>
       </Section>
       <Devider />
       <Section>
@@ -27,7 +30,12 @@ export default ({ recipe }: { recipe: Recipe }) => {
         <ReactMarkdown
           components={{
             h2({ children }) {
-              return <Heading level="h2">{children}</Heading>;
+              return (
+                <>
+                  <br />
+                  <Heading level="h2">{children}</Heading>
+                </>
+              );
             },
           }}
         >
