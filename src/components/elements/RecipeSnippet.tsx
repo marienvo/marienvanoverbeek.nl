ec5d4648461898eb4808944730f6d5e36d7999d5
marienvanoverbeek <git@marienvanoverbeek.nl>
2024-07-08 17:39:19 +0200

    Add tooltips for health indicators

--- src/components/elements/RecipeSnippet.tsx
+++ src/components/elements/RecipeSnippet.tsx
@@ -10,13 +10,56 @@ export const RecipeSnippet: FC<RecipeProperties> = ({ recipe }) => {
   return (
     <div style={{ marginBottom: 40 }}>
       <Heading level="h2">
-        <a href={`/${settings.recipeSlug}/${recipe.slug}`}>
-          {recipe.title}
-          <sup>{(recipe.meta.vegan && `🌿✨`) || (recipe.meta.vega && `🌱`)}</sup>
-        </a>
+        <a href={`/${settings.recipeSlug}/${recipe.slug}`}>{recipe.title}</a>
+
+        {(recipe.meta.vegan && (
+          <sup
+            style={{ cursor: "default" }}
+            title="Fully plant-based, 100% vegan"
+          >
+            🌿✨
+          </sup>
+        )) ||
+          (recipe.meta.vega && (
+            <sup style={{ cursor: "default" }} title="Suitable for vegetarians">
+              🌱
+            </sup>
+          ))}
       </Heading>
-      <ReactMarkdown>{recipe.intro}</ReactMarkdown>
-      <p>Health score: {recipe.meta.healthy}</p>
+      <ReactMarkdown
+        components={{
+          em({ children }) {
+            const getLabel = (value: string) => {
+              switch (value) {
+                case "🥬🥬🥬":
+                  return "Your body will thank you!";
+                case "🥬🥬":
+                  return "A solid choice!";
+                case "🥬":
+                  return "Good in moderation!";
+                case "⚖️":
+                  return "Not the worst, but not the healthiest either";
+                case "🍔":
+                  return "Treat yourself, but don't overdo it!";
+                case "🍔🍔":
+                  return "Tasty for sure, but limit your intake!";
+                case "🍔🍔🍔":
+                  return "Handle with care!";
+                default:
+                  return undefined;
+              }
+            };
+            return (
+              <span
+                style={{ cursor: "default" }}
+                title={getLabel(children.toString())}
+              >
+                {children}
+              </span>
+            );
+          },
+        }}
+      >{`${recipe.intro} _${recipe.meta.healthy}_`}</ReactMarkdown>
     </div>
   );
 };
