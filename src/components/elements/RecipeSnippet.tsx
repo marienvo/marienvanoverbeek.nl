41350f6866dc9b557ebbadb1dd6dd2dc0ae5dede
marienvanoverbeek <git@marienvanoverbeek.nl>
2024-07-08 13:42:08 +0200

    Fix recipe

--- src/components/elements/RecipeSnippet.tsx
+++ src/components/elements/RecipeSnippet.tsx
@@ -12,7 +12,7 @@ export const RecipeSnippet: FC<RecipeProperties> = ({ recipe }) => {
       <Heading level="h2">
         <a href={`/${settings.recipeSlug}/${recipe.slug}`}>
           {recipe.title}
-          <sup>{recipe.meta.score}{recipe.meta.vega && ` 🌿`}{recipe.meta.vegan && ` ✅`}</sup>
+          <sup>{(recipe.meta.vegan && `🌿✨`) || (recipe.meta.vega && `🍃`)}</sup>
         </a>
       </Heading>
       <ReactMarkdown>{recipe.intro}</ReactMarkdown>
